bfd5dd446058a9fbc13640ff0a3edc19f3466da8
zaahirp <zaahirp@frci.net>
2022-01-25 10:47:16 +0400

    First Commit - Create Project

---
+++ src/webparts/addAssets/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IAddAssetsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'AddAssetsWebPartStrings' {
+  const strings: IAddAssetsWebPartStrings;
+  export = strings;
+}
